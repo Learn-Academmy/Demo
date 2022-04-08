148f394e348e2c89d5cf4261fb77b18257a63637
Learn-Academmy <98627649+Learn-Academmy@users.noreply.github.com>
2022-04-08 14:38:07 -0300

    zxcz
    xczxc

--- Reloj.ino
+++ Reloj.ino
@@ -19,7 +19,6 @@ void setup()
 void loop()
 {
     DateTime fecha = rtc.now(); // Obtenemos la hora actual y la almacenamos en un objeto llamado fecha
-    Serial.println(fecha); // Imprimimos la fecha y hora actual
     Serial.println(fecha.hour()); // Imprimimos la fecha y hora actual
     Serial.println(fecha.minutes()); // Imprimimos la fecha y hora actual
     Serial.println(decha.seconds()); // Imprimimos la fecha y hora actual
